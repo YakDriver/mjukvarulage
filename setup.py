dcdbbf3b628995baca977e2f67da2341e4714a7b
Dirk Avery <dirk.avery@gmail.com>
2018-04-27 12:35:08 -0400

    experiments

--- setup.py
+++ setup.py
@@ -5,10 +5,7 @@ from setuptools import setup
 
 if __name__ == "__main__":
 
-    with open('README.rst', 'r') as f:
-        long_description = f.read()
-
     setup(
-        long_description=long_description,
-        package_dir={'': str('src')}
+        package_dir={'': str('src')},
+        author = "YakDriver"
     )
